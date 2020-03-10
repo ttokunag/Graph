ada1d1875599896539a55fde88e0ac7f5233ce0c
ttokunag <ttokunag@ucsd.edu>
2020-03-10 04:30:27 +0000

    commented on test_Map

--- test/test_Map.cpp
+++ test/test_Map.cpp
@@ -1,10 +1,13 @@
+/**
+ * This file test Map class
+ */
+
 #include <gtest/gtest.h>
 #include "Map.hpp"
 
 using namespace std;
 using namespace testing;
 
-// TODO: add tests for map graph
 TEST(MapTest, DIJKSTRA_TEST) {
     Map* map = new Map();
     string vertexFile = "../src/map1xy.txt";
@@ -13,6 +16,30 @@ TEST(MapTest, DIJKSTRA_TEST) {
 
     vector<Vertex*> shortestPath;
     map->Dijkstra("Warren", "Revelle", shortestPath);
+
+    delete map;
+}
+
+TEST(MapTest, KRUSKAL_TEST) {
+    Map* map = new Map();
+    string vertexFile = "../src/map1xy.txt";
+    string edgeFile = "../src/map1roads.txt";
+    map->buildMapFromFile(vertexFile, edgeFile);
+
+    vector<Edge*> MSTEdges;
+    map->findMST(MSTEdges);
+
+    delete map;
+}
+
+TEST(MapTest, CRUCIAL_ROADS_TEST) {
+    Map* map = new Map();
+    string vertexFile = "../src/map1xy.txt";
+    string edgeFile = "../src/map1roads.txt";
+    map->buildMapFromFile(vertexFile, edgeFile);
+
+    vector<Edge*> crucialRoads;
+    map->crucialRoads(crucialRoads);
 
     delete map;
 }
